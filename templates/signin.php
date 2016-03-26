0283429a53e841278896c7694a6ceb29696a0cb9
Luke Cahill <lmc@outlook.com>
2016-03-26 20:01:46 +0000

    Change to the links

--- templates/signin.php
+++ templates/signin.php
@@ -1,8 +1,12 @@
 <div class="col-sm-12 text-right">
 	<span class="login-link">
-		Log in
+		<a href="login.php">
+			Log in
+		</a>
 	</span>
 	<span class="register-link">
-		Register
+		<a href="register.php">
+			Register
+		</a>
 	</span>
 </div>
