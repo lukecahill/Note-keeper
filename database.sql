14e18d575e16d1d6b03c92f00cd635946a77aa6f
Luke Cahill <lmc@outlook.com>
2016-03-26 15:41:47 +0000

    Database changes - now includes a user table, and the note can be marked as complete.

--- database.sql
+++ database.sql
@@ -4,5 +4,12 @@ CREATE TABLE note (
     NoteId INT(11) AUTO_INCREMENT PRIMARY KEY,
     NoteText VARCHAR(255),
     NoteTags VARCHAR(512),
-	NoteTitle VARCHAR(50)
+	NoteTitle VARCHAR(50),
+	NoteComplete TINYINT
+);
+
+CREATE TABLE note_users (
+    UserId INT(11) AUTO_INCREMENT PRIMARY KEY,
+    UserEmail VARCHAR(255),
+    UserPassword VARCHAR(512)
 );
