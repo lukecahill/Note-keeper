0c0ee4c67c2c929a0686a633e17c0e84a58b5e7e
Luke Cahill <lmc@outlook.com>
2016-03-26 20:02:36 +0000

    Header and footer now in their own files, with additional markup which is shared with other pages.

---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Notes
+	</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="jumbotron">
+		<h1>
+			<!-- <span class="glyphicon glyphicon-comment"> -->
+			<img src="images/note.png" class="note-logo"></img>
+			</span>
+				Note Keeper.
+		</h1>
+	</div>
