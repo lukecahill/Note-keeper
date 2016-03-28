6221014530468a7a22bd6b64b6e267f0550914e7
Luke Cahill <lmc@outlook.com>
2016-03-28 15:58:06 +0100

    Updated SQL for the Foreign key constraint. Change the UserId to VARCHAR, as this will be a hash of the users email.

--- database.sql
+++ database.sql
@@ -5,11 +5,13 @@ CREATE TABLE note (
     NoteText VARCHAR(255),
     NoteTags VARCHAR(512),
 	NoteTitle VARCHAR(50),
-	NoteComplete TINYINT
+	NoteComplete TINYINT,
+	UserId VARCHAR(60),
+	FOREIGN KEY (UserId) REFERENCES note_users(UserId) ON DELETE CASCADE
 );
 
 CREATE TABLE note_users (
-    UserId INT(11) AUTO_INCREMENT PRIMARY KEY,
+    UserId VARCHAR(60) AUTO_INCREMENT PRIMARY KEY,
     UserEmail VARCHAR(255),
     UserPassword VARCHAR(512)
 );
