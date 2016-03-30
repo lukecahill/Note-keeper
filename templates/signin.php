853c213d87644c9aab9f8a8b236a963fb829e116
Luke Cahill <lmc@outlook.com>
2016-03-30 18:30:57 +0100

    Change the singin to change the options depending on the page.

--- templates/signin.php
+++ templates/signin.php
@@ -1,20 +1,48 @@
 <div class="col-sm-12 text-right">
 
 <?php if(isset($_SESSION['user'])) { ?>
+	<?php 
+		$url = $_SERVER['REQUEST_URI']; 
+		if(!strpos($url, 'options.php')) { 
+	?>
+		<span class="options-link">
+			<a href="options.php">
+				Options
+			</a>
+		</span>	
+	<?php
+		} else {
+	?>
+		<span class="index-link">
+			<a href="index.php">
+				Home
+			</a>
+		</span>	
+	<?php } ?>
+	
+	<span class="divider">
+		|
+	</span>
+	
 	<span class="logout-link">
 		<a href="includes/logout.php">
 			Logout
 		</a>
 	</span>
 <?php } else { ?>
-	<span class="login-link">
-		<a href="login.php">
-			Log in
-		</a>
-	</span>
 	<span class="register-link">
 		<a href="register.php">
 			Register
+		</a>
+	</span>
+	
+	<span>
+		|
+	</span>
+	
+	<span class="login-link">
+		<a href="login.php">
+			Log in
 		</a>
 	</span>
 <?php } ?>
