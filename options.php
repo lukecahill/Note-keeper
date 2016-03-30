511afa7e487d053363e28ff2399a26c4d7b6e8da
Luke Cahill <lmc@outlook.com>
2016-03-30 18:33:00 +0100

    Added options.php page where the user can view their account and change their settings.

---
+++ options.php
@@ -0,0 +1,45 @@
+<?php include 'templates/header.php'; ?>
+	
+<div class="col-sm-12 row">
+
+	<?php include 'templates/signin.php'; ?>
+	<div class="col-sm-12">
+		<h2>
+			User Options 
+		</h2>
+		
+		<p>
+			Modify your options to your preferences. 
+		</p>
+		
+		<p>
+			<b>
+				Email:
+			</b>
+			<?php echo $_SESSION['user']; ?>
+		</p>
+		
+		<p>
+			<b>
+				Enter a color for your tags (enter as hexidecimal code e.g. #FF0000):
+			</b>
+			<div class="form-group">
+				<label for="user-tag-color">
+					Hexadecimal color code
+				</label>
+				<input type="text" id="user-tag-color" class="form-control">
+			</div>
+			<button class="btn btn-success" id="tag-color-button">
+				<span class="glyphicon glyphicon-plus"></span>
+				Change Color
+			</button>
+		</p>
+	</div>
+</div>
+
+<?php echo '<script>var userId = "' . $_SESSION['userId'] . '"; </script>'; // use this to echo the session user ID for the JS to use ?> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+<script src="js/user-script.js"></script>
+
+<?php include 'templates/footer.html'; ?>
