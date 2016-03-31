eec82ff57d3aab09be895bc6b3b5707f88b4c798
Luke Cahill <lmc@outlook.com>
2016-03-31 21:55:00 +0100

    Change the SQL statement to update all fields.

--- includes/edit-note.php
+++ includes/edit-note.php
@@ -2,14 +2,18 @@
 
 if(isset($_POST['noteText']) && isset($_POST['noteId']) && $_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
 	
-	$note = $_POST['noteText'];
 	$noteId = $_POST['noteId'];
+	$noteText = $_POST['noteText'];
+	$noteTags = isset($_POST['noteTags']) ? $_POST['noteTags'] : '';
+	$noteTitle = $_POST['noteTitle'];
+	
+	$noteTags = serialize($noteTags);
 	
 	require_once 'db-connect.inc.php';
 	$db = ConnectDb();
 	
-	$stmt = $db->prepare('UPDATE note SET NoteText = :text WHERE NoteId = :id ');
-	$stmt->execute(array( ':text' => $note, ':id' => $noteId));
+	$stmt = $db->prepare('UPDATE note SET NoteText = :text, NoteTitle = :title, NoteTags = :tags WHERE NoteId = :id ');
+	$stmt->execute(array(':text' => $noteText, ':title' => $noteTitle, ':tags' => $noteTags, ':id' => $noteId));
 	
 	echo $noteId . ' has been updated.';
 } else {
