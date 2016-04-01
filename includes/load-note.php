c2731f2123abf38d1f0dcc6882de7d853296e741
Luke Cahill <lmc@outlook.com>
2016-04-01 13:24:58 +0100

    Change load tags so this is done the same time as the note is loaded. PHP file then returns JSON which is then parsed client side.

--- includes/load-note.php
+++ includes/load-note.php
@@ -17,41 +17,67 @@ if(($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['userId']) && isset($_
 	$stmt->execute(array(':userId' => $userId, ':complete' => $complete));
 	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	$count = $stmt->rowCount();
+
+	$tagList = array();
+	$checkbox = array();
+	$merged = array();
+	$notes = array();
 	
 	if($count > 0) {
 		foreach($rows as $item) {
 			
-			$tags = '';
+			$note = '';
 			$tags = unserialize($item['NoteTags']);
 			
-			echo '<div class="note" data-id="' . $item['NoteId'] . '"> 
+			$note .= '<div class="note" data-id="' . $item['NoteId'] . '"> 
 					<span class="note-id" id="' . $item['NoteId'] . '">Note ID: ' . $item['NoteId']. '</span>
 					<h4 class="note-title">' . $item['NoteTitle'] . '</h4>
 					<p class="note-text">' . $item['NoteText'] . '</p>';
 			
 			if(sizeof($tags) > 0 && $tags !== '') {
 				foreach($tags as $tag) {
-					echo '<span class="note-tags" title="Click to show all notes with this tag." data-tag="' . $tag . '">' . $tag . '</span>';
+					$note .= '<span class="note-tags" title="Click to show all notes with this tag." data-tag="' . $tag . '">' . $tag . '</span>';
+				}
+			}
+			
+			if(sizeof($tags) > 0 && $tags !== '') {
+				foreach($tags as $tag) {
+					
+					if(!in_array($tag, $tagList)) { 
+						$tagList[] = $tag;
+						
+						// limit the pre-loaded tags to 5.
+						if(5 >= count($tagList)) {
+							$checkbox[] = $tag;
+						}
+					}
 				}
 			}
 			
 			if($complete == 0) {
-				echo '<div class="note-glyphicons">
+				$note .= '<div class="note-glyphicons">
 					<span class="glyphicon glyphicon-remove remove-note" title="Delete this note"></span>
 					<span class="glyphicon glyphicon-edit edit-note" title="Edit this note"></span>
 					<span class="glyphicon glyphicon-ok note-done" title="Mark as done"></span>
 					</div>';
 			} else {
-				echo '<div class="note-glyphicons">
+				$note .= '<div class="note-glyphicons">
 					<span class="glyphicon glyphicon-remove remove-note" title="Delete this note"></span>
 					<span class="glyphicon glyphicon-asterisk mark-note-active" title="Mark as active"></span>
 					</div>';
 			}
 			
-			echo '</div>';
+			$note .= '</div>';
+			$notes[] = $note;
 		}
 		
-		echo '<style> .note .note-tags { background-color: #' . $rows[0]['TagColor'] . '; border-color: #' . $rows[0]['TagColor'] . ' } </style>';
+		$style = '<style> .note .note-tags { background-color: #' . $rows[0]['TagColor'] . '; border-color: #' . $rows[0]['TagColor'] . ' } </style>';
+		
+		array_push($merged, $checkbox);
+		array_push($merged, $tagList);
+		array_push($merged, $notes);
+		array_push($merged, $style);
+		echo json_encode($merged);
 		
 	} else {
 		echo 'none';
