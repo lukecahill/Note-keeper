35efc552cfeb85de1d31417f3b2891a7147338f7
Luke Cahill <lmc@outlook.com>
2016-04-02 12:39:42 +0100

    Add form to change the users password.

---
+++ templates/password.php
@@ -0,0 +1,58 @@
+<?php
+
+if(isset($_POST['new-password']) && isset($_POST['confirm-password']) && isset($_POST['old-password']) && isset($_SESSION['userId'])) {
+	
+	$old = $_POST['old-password'];
+	$new = $_POST['new-password'];
+	$confirm = $_POST['confirm-password'];
+	$userId = $_SESSION['userId'];
+	
+	require_once 'includes/db-connect.inc.php';
+	$db = ConnectDb();
+	
+	$stmt = $db->prepare('SELECT UserPassword FROM note_users WHERE UserId = :id');
+	$stmt->execute(array(':id' => $userId));
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	
+	$encrypted = $results[0]['UserPassword'];
+	
+	if(password_verify($old, $encrypted)) {
+		$new = password_hash($new, PASSWORD_DEFAULT);
+		$stmt = $db->prepare('UPDATE note_users SET UserPassword = :password WHERE UserId = :id');
+		$stmt->execute(array(':password' => $new,':id' => $userId));
+		echo 'Pasword Changed!';
+	}
+	
+} else if(isset($_POST['change-password-button'])) {
+	echo 'No lols';
+}
+
+?>
+
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="change-password-form">
+	<div class="form-group">
+		<label for="old-password">
+			Enter old password:
+		</label>
+		<input type="text" id="old-password" name="old-password" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="new-password">
+			Enter new password:
+		</label>
+		<input type="text" id="new-password" name="new-password" class="form-control">
+	</div>
+
+	<div class="form-group">
+		<label for="confirm-password">
+			Confirm new password:
+		</label>
+		<input type="text" id="confirm-password" name="confirm-password" class="form-control">
+	</div>
+
+	<button class="btn btn-success" id="change-password-button" name="change-password-button" type="submit">
+		<span class="glyphicon glyphicon-asterisk"></span>
+		Change Password
+	</button>
+</form>
