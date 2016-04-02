6f76a5b027142a0861af4d94697061868b69c7cf
Luke Cahill <lmc@outlook.com>
2016-04-02 19:45:31 +0100

    Restructure the user settings into a class.

--- includes/user-settings.php
+++ includes/user-settings.php
@@ -1,24 +1,54 @@
 <?php
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+	$user = new UserSettings($_POST['action']);
 	
-	if(isset($_POST['id'])) {
-		$color = $_POST['color'];
-		$id = $_POST['id'];
-		
-		require_once 'db-connect.inc.php';
-		$db = ConnectDb();
-		
-		$stmt = $db->prepare('UPDATE note_users SET TagColor = :color WHERE UserId = :id ');
-		$stmt->execute(array( ':color' => $color, ':id' => $id));
-		
-		echo 'Updated!';
+	if($user->method === 'get-tag-color') {
+		$user->getTagColor();
+	} else if($user->method === 'set-tag-color') {
+		$user->setTagColor();
 	}
-	
-	// other user settings can then be updated in an if-else
 
 } else {
 	echo 'No direct access';
+}
+
+class UserSettings {
+	public $method = '';
+	public $id = 0;
+	public $color = '';
+	
+	function __construct($method) {
+		$this->method = $method;
+	}
+	
+	function setTagColor() {
+		if($_POST['id'] !== 0) {
+			$this->color = $_POST['color'];
+			$this->id = $_POST['id'];
+			
+			require_once 'db-connect.inc.php';
+			$db = ConnectDb();
+			
+			$stmt = $db->prepare('UPDATE note_users SET TagColor = :color WHERE UserId = :id ');
+			$stmt->execute(array( ':color' => $this->color, ':id' => $this->id));
+		}
+	}
+	
+	function getTagColor() {
+		if($_POST['id'] !== 0) {
+			$this->id = $_POST['id'];
+			
+			require_once 'db-connect.inc.php';
+			$db = ConnectDb();
+			
+			$stmt = $db->prepare('SELECT TagColor FROM note_users WHERE UserId = :id ');
+			$stmt->execute(array(':id' => $this->id));
+			$color = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			
+			echo json_encode($color[0]['TagColor']);
+		}
+	}
 }
 
 ?>
