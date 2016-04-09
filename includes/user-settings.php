08d9e506504c35fa2c2e1b88b929c32338dd2421
Luke Cahill <lmc@outlook.com>
2016-04-09 15:16:39 +0100

    Add the database as a variable which is assigned in the constructor Edit the class as appropriate to use this new variable.

--- includes/user-settings.php
+++ includes/user-settings.php
@@ -17,9 +17,12 @@ class UserSettings {
 	public $method = '';
 	public $id = 0;
 	public $color = '';
+	public $db = null;
 	
 	function __construct($method) {
 		$this->method = $method;
+		require_once 'db-connect.inc.php';
+		$this->db = Database::ConnectDb();
 	}
 	
 	function setTagColor() {
@@ -27,10 +30,7 @@ class UserSettings {
 			$this->color = $_POST['color'];
 			$this->id = $_POST['id'];
 			
-			require_once 'db-connect.inc.php';
-			$db = ConnectDb();
-			
-			$stmt = $db->prepare('UPDATE note_users SET TagColor = :color WHERE UserId = :id ');
+			$stmt = $this->db->prepare('UPDATE note_users SET TagColor = :color WHERE UserId = :id ');
 			$stmt->execute(array( ':color' => $this->color, ':id' => $this->id));
 		}
 	}
@@ -38,11 +38,8 @@ class UserSettings {
 	function getTagColor() {
 		if($_POST['id'] !== 0) {
 			$this->id = $_POST['id'];
-			
-			require_once 'db-connect.inc.php';
-			$db = ConnectDb();
-			
-			$stmt = $db->prepare('SELECT TagColor FROM note_users WHERE UserId = :id ');
+
+			$stmt = $this->db->prepare('SELECT TagColor FROM note_users WHERE UserId = :id ');
 			$stmt->execute(array(':id' => $this->id));
 			$color = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			
