61d9f13746c6f44c1946fe0298cd0beeb653bf51
Luke Cahill <lmc@outlook.com>
2016-04-09 12:06:42 +0100

    Change the order of the SQL

--- database.sql
+++ database.sql
@@ -1,5 +1,12 @@
 CREATE DATABASE notes;
 
+CREATE TABLE note_users (
+    UserId VARCHAR(60) PRIMARY KEY,
+    UserEmail VARCHAR(255),
+    UserPassword VARCHAR(512),
+    TagColor VARCHAR(6)
+);
+
 CREATE TABLE note (
     NoteId INT(11) AUTO_INCREMENT PRIMARY KEY,
     NoteText VARCHAR(255),
@@ -8,11 +15,4 @@ CREATE TABLE note (
 	NoteComplete TINYINT NULL DEFAULT '0',
 	UserId VARCHAR(60),
 	FOREIGN KEY (UserId) REFERENCES note_users(UserId) ON DELETE CASCADE
-);
-
-CREATE TABLE note_users (
-    UserId VARCHAR(60) PRIMARY KEY,
-    UserEmail VARCHAR(255),
-    UserPassword VARCHAR(512),
-	TagColor VARCHAR(6)
 );
