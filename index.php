3028ee26c5a8b648b595b99cdfef7aad2dca25f7
Luke Cahill <lmc@outlook.com>
2016-04-27 20:00:27 +0100

    Add the JS variable which will store the users chosen color

--- index.php
+++ index.php
@@ -62,6 +62,7 @@
 <?php include 'templates/modals/edit-note.html'; ?>
 <?php echo '<script>var userId = "' . $_SESSION['userId'] . '"; </script>'; // use this to echo the session user ID for the JS to use ?> 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script>var color = 'red';</script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="js/script.js"></script>
