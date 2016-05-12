6b8708c1470bb4290bb74d56dc44e65fea829b79
Luke Cahill <lmc@outlook.com>
2016-05-12 21:23:49 +0100

    Add script to fix mobile menu JS not functioning.

--- options.php
+++ options.php
@@ -28,6 +28,7 @@
 <?php echo '<script>var userId = "' . $_SESSION['userId'] . '"; </script>'; // use this to echo the session user ID for the JS to use ?> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="js/user-script.js"></script>
 
 <?php include 'templates/footer.html'; ?>
