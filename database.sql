94c35e4a7dc1f76a6b14c5485580add23b2796d7
Luke Cahill <lmc@outlook.com>
2016-07-03 18:03:05 +0100

    Change the size of the database fields

--- database.sql
+++ database.sql
@@ -11,9 +11,9 @@ CREATE TABLE note_users (
 
 CREATE TABLE note (
     NoteId INT(11) AUTO_INCREMENT PRIMARY KEY,
-    NoteText VARCHAR(255),
-    NoteTags VARCHAR(512),
-	NoteTitle VARCHAR(50),
+    NoteText VARCHAR(1024),
+    NoteTags VARCHAR(1024),
+	NoteTitle VARCHAR(256),
 	NoteComplete TINYINT NULL DEFAULT '0',
 	UserId VARCHAR(60),
 	FOREIGN KEY (UserId) REFERENCES note_users(UserId) ON DELETE CASCADE
