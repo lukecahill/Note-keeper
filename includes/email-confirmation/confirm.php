88a6b929962871f08121206ddf415bccea8ef556
Luke Cahill <lmc@outlook.com>
2016-07-09 17:44:25 +0100

    Skeleton code for confirming the users email

--- includes/email-confirmation/confirm.php
+++ includes/email-confirmation/confirm.php
@@ -1,5 +1,22 @@
 <?php
 
-echo 'Confirm your email';
+if(isset($_GET['hash']) && !empty($_GET['hash'])) {
+	
+	$hash = $_GET['hash'];
+
+	echo $hash;
+	
+	require_once '../db-connect.inc.php';
+	
+	$db = Database::ConnectDb();
+	$id = $_GET['user'];
+	$stmt = $db->prepare('UPDATE note_users SET Active = 1 WHERE UserId = :id');
+	$stmt->execute(array(':id' => $id));
+	
+	echo $id;
+
+} else {
+	echo 'No email found are you sure you are meant to be here?';
+}
 
 ?>
