5dcba12b6ecd2e2a0d860d950930b7f12a3e0d03
Luke Cahill <lmc@outlook.com>
2016-07-10 17:56:28 +0100

    Change confirmation to be OO

--- includes/email-confirmation/confirm.php
+++ includes/email-confirmation/confirm.php
@@ -1,20 +1,45 @@
 <?php
 
+class ConfirmEmail {
+	public $hash = '';
+	public $id = '';
+	public $db = null;
+	
+	function __construct() {
+		require_once '../db-connect.inc.php';
+		$this->db = Database::ConnectDb();
+		$this->id = $id;
+		$this->hash = $hash;
+	}
+	
+	function getId() {
+		$stmt = $this->db->prepare('SELECT UserId FROM note_users WHERE UserId = :id AND EmailConfirmation = :hash');
+		$stmt->execute(array(':id' => $this->id, ':hash' => $this->hash));
+		
+		if($stmt->rowCount() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	function checkHash() {
+		$stmt = $this->db->prepare('UPDATE note_users SET Active = 1 WHERE UserId = :id');
+		$stmt->execute(array(':id' => $this->id));
+	}
+}
+
 if(isset($_GET['hash']) && !empty($_GET['hash'])) {
 	
-	$hash = $_GET['hash'];
-
-	echo $hash;
+	$confirm = new ConfirmEmail($_GET['hash'], $_GET['userId']);
+	$valid = $confirm->getId();
 	
-	require_once '../db-connect.inc.php';
+	if($valid) {
+		$confirm->checkHash();	
+	} else {
+		echo 'The user ID or the hash which was entered was incorrect. Please check your email.';
+	}
 	
-	$db = Database::ConnectDb();
-	$id = $_GET['user'];
-	$stmt = $db->prepare('UPDATE note_users SET Active = 1 WHERE UserId = :id');
-	$stmt->execute(array(':id' => $id));
-	
-	echo $id;
-
 } else {
 	echo 'No email found are you sure you are meant to be here?';
 }
