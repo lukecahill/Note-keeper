680d5fbf1ed3b932b072a359fd9a88c82ad1f3e3
Luke Cahill <lmc@outlook.com>
2016-07-11 17:46:04 +0100

    Fix the incorrect if statement and return

--- includes/email-confirmation/confirm.php
+++ includes/email-confirmation/confirm.php
@@ -5,8 +5,8 @@ class ConfirmEmail {
 	public $id = '';
 	public $db = null;
 	
-	function __construct() {
-		require_once '../db-connect.inc.php';
+	function __construct($hash, $id) {
+		require_once 'includes/db-connect.inc.php';
 		$this->db = Database::ConnectDb();
 		$this->id = $id;
 		$this->hash = $hash;
@@ -16,7 +16,7 @@ class ConfirmEmail {
 		$stmt = $this->db->prepare('SELECT UserId FROM note_users WHERE UserId = :id AND EmailConfirmation = :hash');
 		$stmt->execute(array(':id' => $this->id, ':hash' => $this->hash));
 		
-		if($stmt->rowCount() == 0) {
+		if($stmt->rowCount() != 0) {
 			return true;
 		} else {
 			return false;
@@ -26,16 +26,23 @@ class ConfirmEmail {
 	function checkHash() {
 		$stmt = $this->db->prepare('UPDATE note_users SET Active = 1 WHERE UserId = :id');
 		$stmt->execute(array(':id' => $this->id));
+		
+		if($stmt->rowCount() == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
 
-if(isset($_GET['hash']) && !empty($_GET['hash'])) {
+if(isset($_GET['hash']) && !empty($_GET['hash']) && isset($_GET['user']) && !empty($_GET['user'])) {
 	
-	$confirm = new ConfirmEmail($_GET['hash'], $_GET['userId']);
-	$valid = $confirm->getId();
+	$confirm = new ConfirmEmail($_GET['hash'], $_GET['user']);
 	
-	if($valid) {
-		$confirm->checkHash();	
+	if($confirm->getId()) {
+		if($confirm->checkHash()) {
+			echo 'Your email has been confirmed. Please now <a href="login.php">log in</a>.';
+		}
 	} else {
 		echo 'The user ID or the hash which was entered was incorrect. Please check your email.';
 	}
