8cb5a7380eb1a7e00eccd1f8afbc0a62be9b0791
Luke Cahill <lmc@outlook.com>
2016-07-15 18:09:20 +0100

    Fix the mobile breakpoints

--- templates/header.php
+++ templates/header.php
@@ -12,6 +12,11 @@ if(!isset($_SESSION['user']) && !isset($_SESSION['userId'])) {
 	<title>
 		Notes
 	</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+	
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
