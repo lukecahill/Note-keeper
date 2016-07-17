6dfb6f16133c5712fb4bbe71fac6967966406b32
Luke Cahill <lmc@outlook.com>
2016-07-17 22:23:18 +0100

    Change the window title to Note Keeper. Add meta tags to all pages, for mobile responsivness

--- templates/header.php
+++ templates/header.php
@@ -10,10 +10,9 @@ if(!isset($_SESSION['user']) && !isset($_SESSION['userId'])) {
 <html>
 <head>
 	<title>
-		Notes
+		Note Keeper
 	</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
 	
