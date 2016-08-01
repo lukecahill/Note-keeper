e2c8b335b615dd4c4156f064b493853bc976fdb1
Luke Cahill <lmc@outlook.com>
2016-08-01 18:18:40 +0100

    Remove redundant comment

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 
 <?php include 'templates/modals/edit-note.html'; ?>
 <?php echo '<script>var userId = "' . $_SESSION['userId'] . '"; </script>'; // use this to echo the session user ID for the JS to use ?> 
-<?php echo '<script>var auth = "' . $_SESSION['authentication'] . '"; </script>'; // use this to echo the session user ID for the JS to use ?> 
+<?php echo '<script>var auth = "' . $_SESSION['authentication'] . '"; </script>'; ?> 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
