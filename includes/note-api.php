5ac4c562a187e6b86d748d901882e4c9a4894ea2
Luke Cahill <lmc@outlook.com>
2016-08-03 18:06:47 +0100

    Fix the incorrect options being checked when editing, deleteing notes etc.

--- includes/note-api.php
+++ includes/note-api.php
@@ -4,23 +4,29 @@ require_once 'note.php';
 
 class NoteApi extends Note {
 
-	function __construct($id) {
+	
+	function __construct() {
 		parent::__construct();
-		$this->id = $id;
 	}
 
 	function addNote($id, $text, $tags, $title) {
 		$tags = serialize($tags);
 		$text = nl2br($text);
 		$stmt = $this->db->prepare('INSERT INTO note(NoteTitle, NoteText, NoteTags, UserId) VALUES (:title, :text, :tags, :userId)');
-		$stmt->execute(array(':title' => $title, ':text' => $text, ':tags' => $tags, ':userId' => $id));
+		$status = $stmt->execute(array(':title' => $title, ':text' => $text, ':tags' => $tags, ':userId' => $id));
 		
-		echo $this->db->lastInsertId();
+		$return = array();
+		
+		array_push($return, $status);
+		array_push($return, $this->db->lastInsertId());
+		
+		echo json_encode($return);
 	}
 
 	function deleteNote($noteId) {
 		$stmt = $this->db->prepare('DELETE FROM note WHERE NoteId = :noteId');
 		$status = $stmt->execute(array(':noteId' => $noteId));
+		
 		echo $status;
 	}
 
@@ -33,16 +39,16 @@ class NoteApi extends Note {
 		echo $status;
 	}
 	
-	function MarkDone() {
+	function MarkDone($id) {
 		$stmt = $this->db->prepare('UPDATE note SET NoteComplete = 1, NoteLastEdited = CURRENT_TIMESTAMP() WHERE NoteId = :id');
-		$status = $stmt->execute(array(':id' => $this->id));
+		$status = $stmt->execute(array(':id' => $id));
 		echo $status;
 	}
 
-	function MarkActive() {
+	function MarkActive($id) {
 		$stmt = $this->db->prepare('UPDATE note SET NoteComplete = 0, NoteLastEdited = CURRENT_TIMESTAMP() WHERE NoteId = :id');
-		$stmt->execute(array(':id' => $this->id));
-		$status = $stmt->execute(array(':id' => $this->id));
+		$stmt->execute(array(':id' => $id));
+		$status = $stmt->execute(array(':id' => $id));
 		echo $status;
 	}
 }
@@ -50,22 +56,22 @@ class NoteApi extends Note {
 if($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
 	
 	$action = $_POST['action'];
-	$note = new NoteApi($_POST['noteId']);
+	$note = new NoteApi();
 
 	if(isset($_POST['noteText']) && ($action === 'addnote')) {
 		$tags = isset($_POST['noteTags']) ? $_POST['noteTags'] : '';
 		$note->addNote($_POST['userId'], $_POST['noteText'], $tags, $_POST['noteTitle']);
-	} else if(isset($_POST['deleteNote']) && ($action === 'deletenote')) {
-		$note->deleteNote($_POST['deleteNote']);
+	} else if(isset($_POST['noteId']) && ($action === 'deletenote')) {
+		$note->deleteNote($_POST['noteId']);
 	} else if(($action === 'editnote') && isset($_POST['noteText']) && isset($_POST['noteId'])) {
 		$tags = isset($_POST['noteTags']) ? $_POST['noteTags'] : '';
 		$note->editNote($_POST['noteId'], $_POST['noteText'], $_POST['noteTitle'], $tags);
 	} else if((isset($_POST['complete'])) && ($action === 'setcomplete')) {
 		$complete = $_POST['complete'];
 		if($complete == 1) {
-			$note->MarkDone();
+			$note->MarkDone($_POST['noteId']);
 		} else {
-			$note->MarkActive();
+			$note->MarkActive($_POST['noteId']);
 		}
 	}
 
