32511e3e35cb6b7d6b6d41c70c91ef5da6d4e3ef
Luke Cahill <lmc@outlook.com>
2016-08-06 11:27:20 +0100

    Update the connection method to separate the parameters, and set attributes on the PDO

--- includes/db-connect.inc.php
+++ includes/db-connect.inc.php
@@ -2,7 +2,19 @@
 
 class Database {
 	public static function ConnectDb() {
-		return new PDO('mysql:host=localhost;dbname=notes;charset=utf8', 'root', '');
+		$host = 'localhost';
+		$db = 'notes';
+		$user = 'root';
+		$pass = '';
+		$charset = 'utf8';
+		
+		$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+		
+		$pdo = new PDO($dsn, $user, $pass);
+		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $pdo;
 	}
 }
 
