6269037c924713c29aa5bce21b336226862e2488
Luke Cahill <lmc@outlook.com>
2016-08-07 19:55:44 +0100

    Change the PDO options.

--- includes/db-connect.inc.php
+++ includes/db-connect.inc.php
@@ -11,7 +11,7 @@ class Database {
 		$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 		
 		$pdo = new PDO($dsn, $user, $pass);
-		$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		//$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $pdo;
