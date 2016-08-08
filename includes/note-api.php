8c92ae5bb7ea8792112c83fc549f281e07910e96
Luke Cahill <lmc@outlook.com>
2016-08-08 18:25:10 +0100

    Add comments to the note api class

--- includes/note-api.php
+++ includes/note-api.php
@@ -5,10 +5,24 @@ require_once 'note.php';
 class NoteApi extends Note {
 
 	
+	/**  
+	* Construct the NoteApi class
+	*
+	*/
 	function __construct() {
 		parent::__construct();
 	}
 
+	/**  
+	* Adds the note to the database
+	*
+	* @param string $id The MD5 hashed value of the users ID
+	* @param string $text The text from the new note
+	* @param string $tags The array of the tags the user has added to the note.
+	* @param string $title The title of the new note
+	*
+	* @return void
+	*/
 	function addNote($id, $text, $tags, $title) {
 		$tags = serialize($tags);
 		$text = nl2br($text);
@@ -23,6 +37,13 @@ class NoteApi extends Note {
 		echo json_encode($return);
 	}
 
+	/**  
+	* Delete the note the user has chosen.
+	*
+	* @param string $noteId The ID of the note to update
+	*
+	* @return void
+	*/
 	function deleteNote($noteId) {
 		$stmt = $this->db->prepare('DELETE FROM note WHERE NoteId = :noteId');
 		$status = $stmt->execute(array(':noteId' => $noteId));
@@ -30,6 +51,16 @@ class NoteApi extends Note {
 		echo $status;
 	}
 
+	/**  
+	* Edits the users note in the database
+	*
+	* @param string $id The MD5 hashed value of the users ID
+	* @param string $text The text from the new note
+	* @param string $title The title of the new note
+	* @param string $tags The array of the tags the user has added to the note.
+	*
+	* @return void
+	*/
 	function editNote($id, $text, $title, $tags) {
 		
 		$tags = serialize($tags);
@@ -40,12 +71,26 @@ class NoteApi extends Note {
 		echo $status;
 	}
 	
+	/**  
+	* Update the note the user has marked as complete
+	*
+	* @param string $id The ID of the note marked as done
+	*
+	* @return void
+	*/
 	function MarkDone($id) {
 		$stmt = $this->db->prepare('UPDATE note SET NoteComplete = 1, NoteLastEdited = CURRENT_TIMESTAMP() WHERE NoteId = :id');
 		$status = $stmt->execute(array(':id' => $id));
 		echo $status;
 	}
 
+	/**  
+	* Update the note the user has marked as active
+	*
+	* @param string $id The ID of the note marked as active
+	*
+	* @return void
+	*/
 	function MarkActive($id) {
 		$stmt = $this->db->prepare('UPDATE note SET NoteComplete = 0, NoteLastEdited = CURRENT_TIMESTAMP() WHERE NoteId = :id');
 		$stmt->execute(array(':id' => $id));
