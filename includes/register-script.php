2572d5e9661eb9715c3e277b0cdf1d15e1471292
Luke Cahill <lmc@outlook.com>
2016-08-10 19:58:50 +0100

    Comment the register script

--- includes/register-script.php
+++ includes/register-script.php
@@ -8,12 +8,24 @@ class Register extends Authentication {
 	public $passwordHash = '';
 	public $confirm = '';
 	public $error = '';
-
+	
+	/**  
+	* Construct the register class
+	*
+	* @param string $email The users email which was used to log in
+	* @param string $password The password the user entered when logging in
+	* @param string $confirm The confirmation password entered by the user.
+	*/
 	function __construct($email, $password, $confirm) {
 		parent::__construct($email, $password);
 		$this->confirm = $confirm;
 	}
 	
+	/**  
+	* Verify that the user entered an email, password and confirmation password.
+	*
+	* @return bool Depending on if the passwords entered were not empty,a nd the user entered an email
+	*/
 	function verify() {
 		if(empty($this->email)) {
 			$this->error = '<span class="validation-error">Please enter your email!</span>';
@@ -33,16 +45,31 @@ class Register extends Authentication {
 		return true;
 	}
 
+	/**  
+	* Create an MD5 hash of the users ID
+	*
+	* @return string MD5 hash of the users email for thier ID
+	*/
 	function hashEmail() {
 		$this->emailHash = md5($this->email);
 		return $this->emailHash;
 	}
 
+	/**  
+	* Uses PHP 5.5+ password_hash() function which hashes the users password
+	*
+	* @return string Hash of the users password
+	*/
 	function hashPassword() {
 		$this->passwordHash = password_hash($this->password, PASSWORD_DEFAULT);
 		return $this->passwordHash;
 	}
 
+	/**  
+	* Check the database to see if a user has already been created with the entered email.
+	*
+	* @return bool True if the email doesn't exit and false if the email already exists.
+	*/
 	function checkExists() {
 		$check = $this->db->prepare('SELECT UserEmail FROM note_users WHERE UserEmail = :email LIMIT 1');
 		$check->execute(array(':email' => $this->email));
@@ -53,6 +80,11 @@ class Register extends Authentication {
 		}
 	}
 
+	/**  
+	* Add the user to the database with their hashed password, email, and ID
+	*
+	* @return bool True if the statement is successful, false if not.
+	*/
 	function addUser() {
 		$this->hashEmail();
 		$this->hashPassword();
@@ -66,11 +98,22 @@ class Register extends Authentication {
 		}
 	}
 	
+	/**  
+	* Creates a record for the users default preferences in the user_preferences table
+	*
+	* @return void
+	*/
 	function createUserPreferences() {
 		$stmt = $this->db->prepare('INSERT INTO user_preferences (UserId) VALUES (:id)');
 		$stmt->execute(array(':id' => $this->emailHash));
 	}
 	
+	/**  
+	* Creates a new emailer to email the user their confirmation link so that they can 
+	* 	confirm their email address. 
+	*
+	* @return void
+	*/
 	function sendConfirmation() {
 		$confirm = new Email($this->email, $this->emailHash, $this->db);
 		$confirm->constructConfirmLink();
