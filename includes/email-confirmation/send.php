c6382608dde0a11915cbaddb7fd82cbe7755d115
Luke Cahill <lmc@outlook.com>
2016-09-14 18:22:24 +0100

    Add item to constructor for if this is a password reset email or not. If statement to then handle this.

--- includes/email-confirmation/send.php
+++ includes/email-confirmation/send.php
@@ -7,24 +7,33 @@ class Email {
 	public $message = '';
 	public $userId = '';
 	public $headers = "From: luke@lcahill.co.uk \r\n Reply-To: luke@lcahill.co.uk \r\n";
+	public $reset = false;
 
-	function __construct($email, $userId, $db) {
+	function __construct($email, $userId, $db, $reset = false) {
 		$this->email = $email;
 		$this->userId = $userId;
 		$this->db = $db;
+		$this->reset = $reset;
 	}
 
 	function constructConfirmLink() {
 		$date = new DateTime();
 		$timestamp = $date->getTimestamp();
 		$confirm = md5($timestamp);
+		$link = '';
 
 		$stmt = $this->db->prepare("UPDATE note_users SET EmailConfirmation = :confirm WHERE UserId = :userId");
 		$stmt->execute(array(':confirm' => $confirm, ':userId' => $this->userId));
 
 		$url = $_SERVER['SERVER_NAME'];
-		$link = 'http://' . $url . '/notes/confirm.php?hash=' . $confirm . '&user=' . $this->userId;
-		$this->message = 'Please follow this link to confirm your account ' . $link;
+		if($this->reset) {
+			$link = 'http://' . $url . '/notes/reset.php?hash=' . $confirm . '&user=' . $this->userId;
+			$this->message = 'Please follow this link to confirm your account ' . $link;
+		} else {
+			$link = 'http://' . $url . '/notes/confirm.php?hash=' . $confirm . '&user=' . $this->userId;
+			$this->message = 'Please follow this link to confirm your account ' . $link;
+		}
+		
 		$this->sendEmail();
 	}
 
