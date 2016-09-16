57e22bab5eb5adc769c982194b17dd6fb563520b
Luke Cahill <lmc@outlook.com>
2016-09-16 20:00:03 +0100

    Add markup for the password reset page.

--- reset.php
+++ reset.php
@@ -1,5 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Note Keeper
+	</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="jumbotron">
+		<h1>
+			<img src="images/note.png" class="note-logo"></img>
+				Note Keeper.
+		</h1>
+	</div>
+	
+	<div class="col-md-12 row">
+		<h3>
+			Reset your password.
+		</h3>
+		<?php include 'includes/PasswordConfirmation.php'; ?>
+	</div>
+	
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
-// TODO...
-
-?>
+<?php include 'templates/footer.html'; ?>
