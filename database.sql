d33ef1dae2e86196d83b0c4b301a35d6f9ccf651
Luke Cahill <lmc@outlook.com>
2016-09-17 14:29:39 +0100

    Change strings to integers in DEFAULT

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@ CREATE TABLE note_users (
     UserEmail VARCHAR(255),
     UserPassword VARCHAR(512),
     EmailConfirmation CHAR(32),
-	Active TINYINT DEFAULT '0',
+	Active TINYINT DEFAULT 0,
 	RecentIps VARCHAR(255),
 	JsonAuthentication VARCHAR(128),
 	PasswordAttempts TINYINT DEFAULT 0
@@ -17,7 +17,7 @@ CREATE TABLE user_preferences (
 	PreferenceId INT(4) PRIMARY KEY AUTO_INCREMENT,
 	UserId VARCHAR(60),
     TagColor VARCHAR(6) DEFAULT 'red',
-	Pagination TINYINT DEFAULT '1',
+	Pagination TINYINT DEFAULT 0,
 	NoteOrder VARCHAR(15) DEFAULT 'oldest',
 	SearchOptions VARCHAR(80) DEFAULT 'a:3:{i:0;s:4:"true";i:1;s:4:"true";i:2;s:5:"false";}',
 	FOREIGN KEY (UserId) REFERENCES note_users(UserId) ON DELETE CASCADE
@@ -28,7 +28,7 @@ CREATE TABLE note (
     NoteText VARCHAR(1024),
     NoteTags VARCHAR(1024),
 	NoteTitle VARCHAR(256),
-	NoteComplete TINYINT NULL DEFAULT '0',
+	NoteComplete TINYINT NULL DEFAULT 0,
 	UserId VARCHAR(60),
 	NoteLastEdited TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
 	FOREIGN KEY (UserId) REFERENCES note_users(UserId) ON DELETE CASCADE
