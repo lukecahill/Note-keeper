9129ef7e1e8b5b640c541b2b7713ed93aa84d637
Luke Cahill <lmc@outlook.com>
2016-09-18 19:15:21 +0100

    Add client-side scripts to pages.

--- password-reset.php
+++ password-reset.php
@@ -1,3 +1,4 @@
+<?php require_once 'includes/PasswordReset.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
--- reset.php
+++ reset.php
@@ -28,7 +28,8 @@
 		<?php include 'includes/PasswordConfirmation.php'; ?>
 	</div>
 	
-<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="js/password-reset-confirm.js"></script>
 
 <?php include 'templates/footer.html'; ?>
