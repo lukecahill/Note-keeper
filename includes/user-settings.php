e66504187c87aeaaa742d400282c3498a5ffceb2
Luke Cahill <lmc@outlook.com>
2016-09-25 11:26:33 +0100

    Add method for updating the theme. Gets this option from the database.

--- includes/user-settings.php
+++ includes/user-settings.php
@@ -11,6 +11,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['HTTP_X_REQUESTED_WITH'] == 
 		$user->setNoteOrder();
 	} else if($user->method === 'set-search-parameters') {
 		$user->setSearchParameters();
+	} else if($user->method === 'set-theme') {
+		$user->setTheme();
 	}
 
 } else {
@@ -88,6 +90,17 @@ class UserSettings {
 		$stmt = $this->db->prepare('UPDATE user_preferences SET SearchOptions = :options WHERE UserId = :id');
 		$stmt->execute(array(':options' => $arr, ':id' => $this->id));
 	}
+
+	/**
+	* Update the database with the users chosen color scheme - currently supports dark or light.
+	* 
+	* @return void
+	**/
+	function setTheme() {
+		$color = $_POST['color'];
+		$stmt = $this->db->prepare('UPDATE user_preferences SET ColorTheme = :color WHERE UserId = :id');
+		$stmt->execute(array(':color' => $arr, ':id' => $this->id));
+	}
 	
 	/**  
 	* Gets the users settings from their preferences in the database.
@@ -95,11 +108,11 @@ class UserSettings {
 	* @return void
 	*/	
 	function getSettings() {
-		$stmt = $this->db->prepare('SELECT p.TagColor, p.NoteOrder, p.SearchOptions, u.RecentIps
+		$stmt = $this->db->prepare('SELECT p.TagColor, p.NoteOrder, p.SearchOptions, u.RecentIps, p.ColorTheme
 									FROM user_preferences p
 									INNER JOIN note_users u ON p.UserId = u.UserId
 									WHERE p.UserId = :id 
-									');
+								');
 		$stmt->execute(array(':id' => $this->id));
 		$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		
@@ -129,7 +142,8 @@ class UserSettings {
 		$text = $searchOptions[1];
 		$complete = $searchOptions[2];
 		$count = $count[0]['total'];
-		$return = array($color, $order, $title, $text, $complete, $count, $recentIps);
+		$theme = $row[0]['ColorTheme'];
+		$return = array($color, $order, $title, $text, $complete, $count, $recentIps, $theme);
 		echo json_encode($return);
 	}
 }
