ae50b4c6b906b9f7dfb5323992bbb76151abf678
Luke Cahill <lmc@outlook.com>
2016-11-15 19:51:31 +0000

    Set localstorage for color theme from users preferences

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 <?php include 'templates/modals/edit-note.html'; ?>
 <?php echo '<script>var userId = "' . $_SESSION['userId'] . '"; </script>'; // use this to echo the session user ID for the JS to use ?> 
 <?php echo '<script>var auth = "' . $_SESSION['authentication'] . '"; </script>'; ?> 
-<?php if(isset($_SESSION['theme'])) echo '<script>var theme = "' . $_SESSION['theme'] . '"; </script>'; ?> 
+<?php if(isset($_SESSION['theme'])) { echo '<script>if(!localStorage.getItem("theme")) { localStorage.setItem("theme", "' . $_SESSION['theme'] . '")}</script>'; } ?> 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
