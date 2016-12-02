f2cf12b3da5a98228930676ccba1e9630cc92ad7
Luke Cahill <lmc@outlook.com>
2016-12-02 15:46:11 +0000

    Fix some maps issues Update CSS

--- js/google-api.js
+++ js/google-api.js
@@ -1,4 +1,6 @@
 var mapLocation = {};
+var map;
+var center;
 
 function initMap() {
     if('geolocation' in navigator) {
@@ -12,17 +14,27 @@ function initMap() {
                 lng: longitude
             };
 
-            var map = new google.maps.Map(document.getElementById('map_location'), {
+            map = new google.maps.Map(document.getElementById('map_location'), {
                 zoom: 4,
-                center: mapLocation
+                center: mapLocation,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
             });
 
             var marker = new google.maps.Marker({
                 position: mapLocation,
                 map: map
             });
+
+            center = map.getCenter();
         });
     } else {
         console.warn('Geolocation is not available');
     }
 }
+
+(function() {
+	$('#maps-header').on('click', function() {
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+	});
+})();
