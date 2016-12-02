169b3dcc7e353b57c052d199c9f5b2053567c6ec
Luke Cahill <lmc@outlook.com>
2016-12-02 13:37:58 +0000

    Fix common.js Remove common elements from individual scripts Update minified items

--- js/script.js
+++ js/script.js
@@ -24,38 +24,6 @@
 	loadNotes(initialLoad);
 
 	/**
-	* @function String format
-	*
-	* Add functionality to String object, for C# style string formatting.
-	* Usage: "{0} is dead, but {1} is alive! {0} {2}".format("ASP", "ASP.NET")
-	* From; http://stackoverflow.com/a/4673436
-	**/
-	if (!String.prototype.format) {
-		String.prototype.format = function() {
-			var args = arguments;
-			return this.replace(/{(\d+)}/g, function(match, number) { 
-				return typeof args[number] != 'undefined' ? args[number] : match;
-			});
-		};
-	}
-	
-	/**
-	* @function localStorageTest
-	*
-	* Checks that the localStorage is enabled on the users browser.
-	**/
-	function localStorageTest() {
-		var test = 'test';
-		try {
-			localStorage.setItem(test, test);
-			localStorage.removeItem(test);
-			return true;
-		} catch(e) {
-			return false;
-		}
-	}
-
-	/**
 	* Check the local storage theme item. 
 	* If the item is light then remove the dark-them class.
 	* If the item is dark then add the dark-theme class onto the HTML.
